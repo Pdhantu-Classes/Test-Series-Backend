e9e5717babc65e372318f2584a1bf15fe41db859
ashishumrey009 <ashishumrey009@gmail.com>
2020-06-29 23:25:55 +0530

    indentation

--- application.py
+++ application.py
@@ -173,6 +173,6 @@ def verify_payment():
     else:
         return json.dumps({"isSuccess":False})
    
- if __name__ == "__main__":
+if __name__ == "__main__":
     app.run(debug = "True",host="0.0.0.0",port=5000)
     # app.run(debug = "True")
