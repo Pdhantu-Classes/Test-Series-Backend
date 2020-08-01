e28891c67b066b913667b78260073c2d65353d56
amit036 <amit68803@gmail.com>
2020-08-01 23:12:51 +0530

    Merge

--- application.py
+++ application.py
@@ -149,12 +149,10 @@ def userLogin():
             print(user_data)
             encoded_jwt = jwt.encode({"user_id": user_data["id"], "firstname": user_data["firstname"],"lastname":user_data["lastname"],"mobile":user_data["mobile"],
                                       "email": user_data["email"], "role": user_data["role"],"module": user_data["module"]}, 'secretkey', algorithm='HS256').decode("utf-8")
-            response = app.response_class(response=json.dumps(
-                {"message": "Login Success", "isValid": True, "token": encoded_jwt, "image_url": user_data["image_url"]}), status=200, mimetype='application/json')
+            response = app.response_class(response=json.dumps({"message": "Login Success", "isValid": True, "token": encoded_jwt, "image_url": user_data["image_url"]}), status=200, mimetype='application/json')
             return response
         else:
-            response = app.response_class(response=json.dumps(
-                {"message": "Wrong Credential", "isValid": False}), status=200, mimetype='application/json')
+            response = app.response_class(response=json.dumps({"message": "Wrong Credential", "isValid": False}), status=200, mimetype='application/json')
             return response
     else:
         response =app.response_class(response=json.dumps({"message":"User do not exists, Please sign up"}),status= 200, mimetype='application/json')
